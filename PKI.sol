97de37fe91b728806196150119715e5c72055501
ychalier <yohan.chalier@telecom-paristech.fr>
2018-04-28 16:09:50 +0200

    first contract implementation & readme update

--- PKI.sol
+++ PKI.sol
@@ -1,101 +1,61 @@
 pragma solidity ^0.4.0;
 
-contract BaseRegistry {
+contract PKI {
 
-    uint public creationTime = now;
-    
-    Record[] public records;
-    Signature[] public signatures;
-    Revocation[] public revocations;
-    
-    // This struct keeps all data for a Record.
-    struct Record {
-        // Keeps the address of this record creator.
-        address issuerID;
-        // Keeps the time when this record was created.
-        uint time;
-        //versionNumber 
-        uint versionNumber;
-        //
-        address clientID;
+    mapping (uint => Certificate) public registry;
+    mapping (uint => Signature) public signings;
+    mapping (uint => bool) public crl;
+    uint nCertificates;
+    uint nSignatures;
+
+    struct Certificate {
+        address issuer;
+        bytes32 publicKey;
         string data;
-        //
-        string datahash;
+        string hash;
     }
+
     struct Signature {
-        address signer;
-        uint recordID;
-        uint end_time;
-    }
-    
-    struct Revocation {
-        uint signatureID;
-        uint revokID;
+        address owner;  // Owner of the signature
+        uint certId;  // Id of the cerficate
+        uint expiry;
     }
 
+    constructor() public { }
 
-    modifier onlyOwner {
-        if (msg.sender != issuerID) throw;
-        _;
+    // Add a certificate
+    function register(address issuer, bytes32 publicKey, string data,
+                      string hash) public returns (uint certId) {
+        certId = nCertificates++;
+        registry[certId] = Certificate(issuer, publicKey, data, hash);
     }
 
-    // This is the function that inserts a record. 
-    function register(uint versionNumber, address clientID, string data, string datahash) returns (uint recordID) {
-        recordID= records.length++;
-        Record record = records[recordID];
-        record.issuerID= msg.sender;
-        record.time= now;
-        record.versionNumber = versionNumber;
-        recod.clientID= clientID;
-        record.data = data;
-        record.datahash= datahash;
-        
+    // Sign a certificate (expiry is time in seconds)
+    function sign(uint certId, uint expiry) public returns (uint signId) {
+        // TODO: should be PKI owner only
+        signId = nSignatures++;
+        signings[signId] = Signature(msg.sender, certId, now + expiry);
+        crl[signId] = false;
     }
-    
-    // Updates the values of the given record.
-    function sign(uint recordID, uint end_time) returns (uint signatureID) {
-        // Only the owner can update his record.
-        if (records[recordID].issuerID == msg.sender) {
-            signatureID = signatures.length++;
-            Signature signature = signatures[signatureID];
-            signature.signer= msg.sender;
-            signature.recordID = recordID;
-            signature.end_time= end_time;
+
+    // Revoke a signature
+    function revoke(uint signId) public {
+        crl[signId] = true;
+    }
+
+    function isSignatureValid(uint signId) public view returns (bool state) {
+        return !crl[signId] && now <= signings[signId].expiry;
+    }
+
+    function isCertificateValid(uint certId) public view returns (bool state) {
+        for (uint i=0; i < nSignatures; i++) {
+            if (signings[i].certId == certId) {
+                if (isSignatureValid(i)) {
+                    return true; // Else try for another signature
+                }
+            }
         }
+        return false;
     }
 
-    // revocation of a given record
-    function revok(uint signatureID) returns (uint revokID) {
-        if (signatures[signatureID].signer == msg.sender) {
-            revokID= revocation.length++;
-            Revocation revocation= revocations[revocationID];
-            revocation.signatureID = signatureID:
-            revocation.revokID= revokID;
-        }
-    }
-
-    // Tells whether a given recordID is registered.
-    function isRegistered(uint recordID) returns(bool) {
-        return records[key].time != 0;
-    }
-
-
-    function getRecord(uint recordID) returns(address issuerID, uint time) {
-        Record record = records[recordID];
-        owner = record.issuerID;
-        time = record.time;
-    }
-    
-    function validityCheck(uint recordID) returns(bool validity) {
-        if (signatures[recordID)
-        validity= 
-        ...
-    }
-
-
-    function kill() onlyOwner {
-        suicide(owner);
-    }
 }
-
-contract is BaseRegistry {}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# SR2I206_PKI
+# PKI implementation on a blockchain
 
+This project aims to implement the tools of a PKI on a blockchain like [Ethereum](https://www.ethereum.org).
+
+## Readings
+
+Here are two articles that helped us building our contract:
+
+ - [PKI Tools in EVM-based Blockchains](https://github.com/WebOfTrustInfo/rebooting-the-web-of-trust/blob/master/topics-and-advance-readings/pki_tools_in_evm_blockchains.md) by [@christianlundkvist](https://github.com/christianlundkvist)
+ - [Privacy based decntralized Public Key Infrastructure (PKI) implementation using Smart contract in Blockchain](https://isrdc.iitb.ac.in/blockchain/workshops/2017-iitb/papers/paper-11%20-%20Decentralized%20PKI%20in%20blockchain%20and%20Smart%20contract.pdf) by Sivakumar P. and Dr. Kunwar Singh
+
+## Tools
+
+ - [Remix](https://remix.ethereum.org/), a Solidity IDE
+ - [Solidity documentation](http://solidity.readthedocs.io/en/develop/index.html)
+
+## Todo-list
+
+ - [ ] Proceed advanced testing
+ - [ ] Ensure security and privacy
+ - [ ] JavaScript API?
